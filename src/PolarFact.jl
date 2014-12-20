8d6d2f122a9f88e46953cd5ccae319cbdf6f0599
Weijian Zhang <weijian.z@hotmail.com>
2014-12-20 18:40:33 +0000

    initialize the interface

--- src/PolarFact.jl
+++ src/PolarFact.jl
@@ -1,5 +1,18 @@
 module PolarFact
 
-# package code goes here
+function polarfact{T}(A::Matrix{T}; 
+                      alg::Symbol=:newton, 
+                      maxiter::Integer=:100,
+                      tol::Real=1.0e-6,
+                      verbose::Bool=false)
+
+    # choose algorithm 
+    algorithm = 
+    alg == :newton ? Newton(maxiter=maxiter, tol=tol, verbose=verbose) :
+    error("Invalid algorithm.")
+
+    # solve for polar factors
+    solve!(algorithm, A, U, H)
+end
 
 end # module
---
+++ src/newton.jl
@@ -0,0 +1,5 @@
+#
+# Newton's method
+#
+# Reference:
+#   
