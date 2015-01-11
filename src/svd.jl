ae06174c4ad810eaef7bef63e375daf7f9962479
Weijian Zhang <weijian.z@hotmail.com>
2015-01-11 09:57:02 +0000

    use svdfact to solve svd decomposition

--- src/svd.jl
+++ src/svd.jl
@@ -7,12 +7,12 @@ immutable SVDAlg <: PolarAlg end
 function solve!(alg::SVDAlg, 
                 X::Matrix{Float64}, U::Matrix{Float64}, H::Matrix{Float64})
 
-    P, S, Q = svd(X, thin = true)
+    F = svdfact(X, thin = true)
     PQt = Array(Float64, size(U))
 
-    A_mul_Bt!(PQt, P, Q)
+    A_mul_Bt!(PQt, F[:U], F[:V])
     copy!(U, PQt)
-    copy!(H, Q * diagm(S) * Q')
+    copy!(H, F[:V] * diagm(F[:S]) * F[:Vt])
     
     H = 0.5 * (H + H')
     return Result(U, H)
