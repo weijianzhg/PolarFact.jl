86454b82c759b9d238243b12f71689914c6ebb92
Weijian Zhang <weijian.z@hotmail.com>
2015-01-11 10:08:41 +0000

    test if A = U*H

--- test/test_newton.jl
+++ test/test_newton.jl
@@ -7,15 +7,18 @@ r = polarfact(A, alg = :newton);
 # Test unitary matrix U
 
 U = r.U
+H = r.H
 
 @test_approx_eq_eps U'*U eye(n) 1e-7
 
 # Test Hermitian positive semifefinite matrix H
 
-@test issym(r.H)
+@test issym(H)
  
-for i in eigvals(r.H)
+for i in eigvals(H)
     @test i >= 0.
 end
+
+@test_approx_eq A U*H
 
 println("Newton method passed test...")
--- test/test_svd.jl
+++ test/test_svd.jl
@@ -8,15 +8,18 @@ r = polarfact(A, alg = :svd);
 # Test unitary matrix U
 
 U = r.U
+H = r.H
 
 @test_approx_eq_eps U'*U eye(n) 1e-7
 
 # Test Hermitian positive semifefinite matrix H
 
-@test issym(r.H)
+@test issym(H)
  
-for i in eigvals(r.H)
+for i in eigvals(H)
     @test i >= 0.
 end
+
+@test_approx_eq A U*H
 
 println("SVD method passed test...")
